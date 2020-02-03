7604212e9fc1b928e0f92b92886982c614607b18
AlexL <agarza@ucsd.edu>
2020-02-03 13:00:09 -0800

    Google Fit connected

--- app/src/main/java/com/example/wwr/MainActivity.java
+++ app/src/main/java/com/example/wwr/MainActivity.java
@@ -1,55 +1,79 @@
 package com.example.wwr;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.wwr.fitness.FitnessService;
+import com.example.wwr.fitness.FitnessServiceFactory;
+import com.example.wwr.fitness.GoogleFitAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+        private static final String TAG = "MainActivity";
+
+        private TextView textSteps;
+        private com.example.wwr.fitness.FitnessService fitnessService;
+
+        private static final String fitnessServiceKey = "GOOGLE_FIT";
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            textSteps = findViewById(R.id.textSteps);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+            FitnessServiceFactory.put(fitnessServiceKey, new FitnessServiceFactory.BluePrint() {
+                @Override
+                public FitnessService create(MainActivity mainActivity) {
+                    return new GoogleFitAdapter(mainActivity);
+                }
+            });
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+            fitnessService = com.example.wwr.fitness.FitnessServiceFactory.create(fitnessServiceKey, this);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            /*Button btnUpdateSteps = findViewById(R.id.buttonUpdateSteps);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            btnUpdateSteps.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {*/
+                    fitnessService.updateStepCount();/*
+                }
+            });*/
+
+            fitnessService.setup();
+
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+
+//       If authentication was required during google fit setup, this will be called after the user authenticates
+            if (resultCode == Activity.RESULT_OK) {
+                if (requestCode == fitnessService.getRequestCode()) {
+                    fitnessService.updateStepCount();
+                }
+            } else {
+                Log.e(TAG, "ERROR, google fit result code: " + resultCode);
+            }
+        }
+
+        public void setStepCount(long stepCount) {
+            textSteps.setText(String.valueOf(stepCount));
+        }
+
 }
