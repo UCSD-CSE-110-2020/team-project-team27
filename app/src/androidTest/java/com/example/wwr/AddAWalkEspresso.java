d1e400a65062a4ea8c6c1f8b0325324295eebebd
AlexL <agarza@ucsd.edu>
2020-02-12 19:55:42 -0800

    Al ver

--- app/src/androidTest/java/com/example/wwr/AddAWalkEspresso.java
+++ app/src/androidTest/java/com/example/wwr/AddAWalkEspresso.java
@@ -1,6 +1,9 @@
 package com.example.wwr;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
@@ -20,6 +23,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Set;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -27,9 +32,11 @@ import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread;
+import static junit.framework.TestCase.assertEquals;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsNot.not;
@@ -37,15 +44,16 @@ import static org.hamcrest.core.IsNot.not;
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class AddAWalkEspresso {
-    private static final String TEST_SERVICE = "TEST_SERVICE";
 
+    private static final String TEST_SERVICE = "TEST_SERVICE";
+    public static final String FITNESS_SERVICE_KEY = "FITNESS_SERVICE_KEY";
 
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<HomeScreenActivity> mActivityTestRule = new ActivityTestRule<>(HomeScreenActivity.class, false, false);
 
     @Test
-    public void AddAWalkDuplicateEspresso() {
-        mActivityTestRule.getActivity().setFitnessServiceKey(TEST_SERVICE);
+    public void startAWalkEspresso() {
+
 
         FitnessServiceFactory.put(TEST_SERVICE, new FitnessServiceFactory.BluePrint() {
             @Override
@@ -54,63 +62,56 @@ public class AddAWalkEspresso {
             }
         });
 
-        mActivityTestRule.getActivity().launchHomeScreenActivity();
+        Intent i = new Intent();
+        i.putExtra(FITNESS_SERVICE_KEY, TEST_SERVICE);
+        mActivityTestRule.launchActivity(i);
 
+        SharedPreferences sp = mActivityTestRule.getActivity().getSharedPreferences("height", Context.MODE_PRIVATE);
 
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.done), withText("DONE"),
-                        childAtPosition(
-                                allOf(withId(R.id.coordinatorLayout2),
-                                        childAtPosition(
-                                                withId(android.R.id.content),
-                                                0)),
-                                1),
-                        isDisplayed()));
-        appCompatButton.perform(click());
+        if(sp.getInt("FEET", 0) == 0){
+            ViewInteraction appCompatButton = onView(
+                    allOf(withId(R.id.done), withText("DONE"),
+                            childAtPosition(
+                                    allOf(withId(R.id.coordinatorLayout2),
+                                            childAtPosition(
+                                                    withId(android.R.id.content),
+                                                    0)),
+                                    1),
+                            isDisplayed()));
+            appCompatButton.perform(click());
+        }
+
+        ViewInteraction pls = onView(allOf(withId(R.id.debugMode)));
+        pls.perform(click());
+
+        pls = onView(allOf(withId(R.id.ClearDataBase_debug)));
+        pls.perform(click());
+
+        pls = onView(allOf(withId(R.id.debugMode)));
+        pls.perform(click());
 
         ViewInteraction appCompatButton2 = onView(
-                allOf(withId(R.id.addButton), withText("+"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                5),
-                        isDisplayed()));
+                allOf(withId(R.id.addButton)));
         appCompatButton2.perform(click());
 
         ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.textView),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
+                allOf(withId(R.id.textView)));
         appCompatEditText.perform(replaceText("a"), closeSoftKeyboard());
 
         ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.textView2),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
+                allOf(withId(R.id.textView2)));
         appCompatEditText2.perform(replaceText("a"), closeSoftKeyboard());
 
         ViewInteraction appCompatButton3 = onView(
-                allOf(withId(R.id.save), withText("SAVE"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                6),
-                        isDisplayed()));
+                allOf(withId(R.id.save)));
         appCompatButton3.perform(click());
 
-        onView(withText("information stored"))
-                .inRoot(withDecorView(not(is(mActivityTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
 
+        sp = mActivityTestRule.getActivity().getSharedPreferences("routeInfo", Context.MODE_PRIVATE);
+
+        Set<String> set = sp.getStringSet("routeNames", null);
+
+        assertEquals(set.contains("a"), true);
     }
 
     private static Matcher<View> childAtPosition(
