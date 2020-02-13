5537cd9b884846135068e4546446792401659dfb
William Hsu <tb950110@gmail.com>
2020-02-12 20:39:13 -0800

    my ver

--- app/src/main/java/com/example/wwr/WalkInfoFromRouteActivity.java
+++ app/src/main/java/com/example/wwr/WalkInfoFromRouteActivity.java
@@ -2,6 +2,7 @@ package com.example.wwr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -24,17 +25,30 @@ public class WalkInfoFromRouteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walk_info_from_route);
 
+
+        String name_i = getIntent().getStringExtra("CLICKED_NAME");
+        SharedPreferences sp = getSharedPreferences("routeInfo", MODE_PRIVATE);
+
         name = findViewById(R.id.Ftitle);
         local = findViewById(R.id.startLoc);
         steps = findViewById(R.id.steps);
-        dist = findViewById();
-        hour = findViewById();
-        min = findViewById();
-        sec = findViewById();
-        features = findViewById();
-        notes = findViewById();
-        start = findViewById();
+        dist = findViewById(R.id.mile);
+        hour = findViewById(R.id.hr);
+        min = findViewById(R.id.min);
+        sec = findViewById(R.id.sec);
+        features = findViewById(R.id.features);
+        notes = findViewById(R.id.notes);
+        start = findViewById(R.id.start);
 
-        String name_i = getIntent().getStringExtra("CLICKED_NAME");
+        /*double dist_double = Double.parseDouble(sp.getString(latestRoute+"_dist", ""));
+        dist_double = Math.round(dist_double * 100.0) / 100.0;
+
+        name.setText(name_i);
+        local.setText(sp.getString(name_i + "_location", ""));
+        steps.setText("" + sp.getInt(name_i + "_steps", 0));
+        dist.setText(Double.toString(dist_double));
+        hour.setText(Integer.toString(routeCount.getInt(latestRoute+"_hour", 0)));
+        min.setText(Integer.toString(routeCount.getInt(latestRoute+"_min", 0)));
+        sec.setText(Integer.toString(routeCount.getInt(latestRoute+"_sec", 0)));*/
     }
 }
