e46cff6d32fc479a82aae1122fc0fea8e48fa1e9
AlexL <agarza@ucsd.edu>
2020-03-01 15:49:06 -0800

    Moved save

--- app/src/main/java/com/example/wwr/AddATeamMemberActivity.java
+++ app/src/main/java/com/example/wwr/AddATeamMemberActivity.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class AddATeamMemberActivity extends AppCompatActivity {
 
     private Button saveTeamMember;
-    private TextView email;
+    private EditText email;
+    private  EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +22,13 @@ public class AddATeamMemberActivity extends AppCompatActivity {
 
         saveTeamMember = findViewById(R.id.saveTeamMember);
         email = findViewById(R.id.emailText);
+        name = findViewById(R.id.nameText);
 
         saveTeamMember.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(emailIsValid(email.getText().toString())) {
-                    UpdateFirebase.inviteTeammate(email.getText().toString());
+                    UpdateFirebase.inviteTeammate(email.getText().toString(), name.getText().toString());
                     finish();
                 }
             }
