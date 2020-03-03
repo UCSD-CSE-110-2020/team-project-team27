30e226188916070f04005eae64bbedaa8ab57988
Ryan <rbez@ucsd.edu>
2020-03-02 18:32:24 -0800

    InviteATeamMemberEspresso.java added, incomplete

---
+++ app/src/androidTest/java/com/example/wwr/InviteATeamMemberEspresso.java
@@ -0,0 +1,211 @@
+package com.example.wwr;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.example.wwr.fitness.FitnessService;
+import com.example.wwr.fitness.FitnessServiceFactory;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread;
+import static junit.framework.TestCase.assertEquals;
+import static org.hamcrest.Matchers.allOf;
+
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class InviteATeamMemberEspresso {
+
+    private static final String TEST_SERVICE = "TEST_SERVICE";
+    public static final String FITNESS_SERVICE_KEY = "FITNESS_SERVICE_KEY";
+
+    @Rule
+    public ActivityTestRule<HomeScreenActivity> mActivityTestRule = new ActivityTestRule<>(HomeScreenActivity.class, false, false);
+
+    @Test
+    public void inviteATeamMemberTest() {
+
+        FitnessServiceFactory.put(TEST_SERVICE, new FitnessServiceFactory.BluePrint() {
+            @Override
+            public FitnessService create(HomeScreenActivity homeScreenActivity) {
+                return new InviteATeamMemberEspresso.TestFitnessService(homeScreenActivity);
+            }
+        });
+
+        User.setEmail("test@gmail.com");
+        User.setName("Test McTesterson");
+
+        FirebaseFirestore mockFirestore = Mockito.mock(FirebaseFirestore.class);
+        CollectionReference mockCol = Mockito.mock(CollectionReference.class);
+        DocumentReference mockDoc = Mockito.mock(DocumentReference.class);
+        CollectionReference mockCol2 = Mockito.mock(CollectionReference.class);
+        DocumentReference mockDoc2 = Mockito.mock(DocumentReference.class);
+        Task mockQ = Mockito.mock(Task.class);
+
+        UpdateFirebase.setDatabase(mockFirestore);
+
+        Mockito.when(mockFirestore.collection("invites")).thenReturn(mockCol);
+        Mockito.when(mockCol.document(User.getEmail())).thenReturn(mockDoc);
+        Mockito.when(mockDoc.collection("team")).thenReturn(mockCol2);
+        Mockito.when(mockCol2.document("a")).thenReturn(mockDoc2);
+        Mockito.when(mockDoc2.set(new HashMap<String, String>())).thenReturn(mockQ);
+
+        //set up another mocked CollectionReference for assert equals comparison
+        CollectionReference mockCol3 = Mockito.mock(CollectionReference.class);
+        Map<String, String> testInvite = new HashMap<>();
+        testInvite.put("Email", User.getEmail());
+        //Sender's name
+        testInvite.put("Name", User.getName());
+        //Receivers name
+        //testInvite.put("Nickname", nickName);
+        mockCol3.add(testInvite);
+
+        Intent i = new Intent();
+        i.putExtra(FITNESS_SERVICE_KEY, TEST_SERVICE);
+        mActivityTestRule.launchActivity(i);
+
+        SharedPreferences sp = mActivityTestRule.getActivity().getSharedPreferences("height", Context.MODE_PRIVATE);
+
+        if(sp.getInt("FEET", 0) == 0){
+            ViewInteraction appCompatButton = onView(
+                    allOf(withId(R.id.done), withText("DONE"),
+                            childAtPosition(
+                                    allOf(withId(R.id.coordinatorLayout2),
+                                            childAtPosition(
+                                                    withId(android.R.id.content),
+                                                    0)),
+                                    1),
+                            isDisplayed()));
+            appCompatButton.perform(click());
+        }
+
+
+
+//        // Added a sleep statement to match the app's execution delay.
+//        // The recommended way to handle such scenarios is to use Espresso idling resources:
+//        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+//        try {
+//            Thread.sleep(700);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        ViewInteraction TeamButton = onView(
+                allOf(withId(R.id.TeamButton)));
+        TeamButton.perform(click());
+
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.teamfab)));
+        floatingActionButton.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.nameText)));
+        appCompatEditText.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.nameText)));
+        appCompatEditText2.perform(replaceText(User.getName()), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.emailText)));
+        appCompatEditText3.perform(replaceText(User.getEmail()), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.saveTeamMember)));
+        appCompatButton3.perform(click());
+
+
+        //assertEquals(mockCol.isEqual(mockCol2), true);
+        //mockFirestore.collection("invites").get();
+        assertEquals(mockFirestore.collection("invites").get() == mockCol3.get(), true);
+
+        }
+
+
+    class TestFitnessService implements FitnessService {
+        private static final String TAG = "[TestFitnessService]: ";
+        private HomeScreenActivity homeScreenActivity;
+
+        public TestFitnessService(HomeScreenActivity homeScreenActivity) {
+            this.homeScreenActivity = homeScreenActivity;
+        }
+
+        @Override
+        public int getRequestCode() {
+            return 0;
+        }
+
+        @Override
+        public void setup() {
+            System.out.println(TAG + "setup");
+        }
+
+        @Override
+        public void updateStepCount(){
+            System.err.println(TAG + "updateStepCount");
+
+
+            try {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        homeScreenActivity.setStepCount(1234);
+                    }
+                });
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        }
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
